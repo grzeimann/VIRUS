d386ec742df26e6112c8b7ccbe8b935e0b1c1e35
grzeimann <grzeimann@gmail.com>
2016-10-11 15:54:28 -0500

    editted fiber class

--- fibers.py
+++ fibers.py
@@ -16,17 +16,21 @@ import numpy as np
 __all__ = ["Fibers"]
 
 class Fibers:
-    def __init__(self, D, order=3):
+    def __init__(self, N, D, trace_poly_order=3):
         ''' 
         Initialize class
         ----------------
+        :param N:
+            The number of rows in the spectrograph detector image.
         :param D:
-            The number of columns in the direction of the trace.
-        :param order:
-            The order of the polynomial used to fit the trace.                
+            The number of columns in the spectrograph detector  image.
+        :param trace_poly_order:
+            The order of the polynomial used to fit the trace.   
+        :init N:
+            Same as described above.             
         :init D:
             Same as described above.
-        :init order:
+        :init trace_poly_order:
             Same as described above.
         :init flag:
             Flag value for undefined values.
@@ -37,23 +41,29 @@ class Fibers:
         :init trace: 
             Polynomial fitted values for the trace.
         :init polyvals:
-            Polynomial coefficients for 
-            
-
+            Polynomial coefficients for trace fit.
+        :init wavelength:
+            Wavelength for each column along the trace.
+        :init throughput:
+            Fiber throughput.
+        :init weight:
+            Fiber Model Weight over the CCD image.
         '''
+        self.N = N
         self.D = D
-        self.order = order
+        self.trace_poly_order = trace_poly_order
         self.flag = -99999
         self.trace_x = self.flag * np.ones((D,),dtype = np.int)
         self.trace_y = np.zeros((D,))
         self.trace = np.zeros((D,))
-        self.polyvals = np.zeros((order,))
-        self.norm = np.zeros((D,))
-        self.deadfiber = False
+        self.polyvals = np.zeros((trace_poly_order,))
+        self.wave = np.zeros((D,))
+        self.throughput = np.zeros((D,))
+        self.weight = np.zeros((N, D))
         
     def fitpoly(self):
         sel = self.x != self.flag
-        self.polyvals = np.polyfit(self.x[sel] / 1032.,self.y[sel],self.order)
+        self.polyvals = np.polyfit(self.x[sel] / 1032.,self.y[sel],self.trace_poly_order)
         
     def evalpoly(self):
         self.trace = np.polyval(self.polyvals, np.arange(len(self.x)) / 1032.)
